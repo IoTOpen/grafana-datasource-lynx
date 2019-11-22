d4053d5d8b963ddda2d7045ca4c83afea101ec53
Tobias Assarsson <tobias.assarsson@gmail.com>
2019-11-22 15:16:08 +0100

    do queries

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -2,8 +2,8 @@ import defaults from 'lodash/defaults';
 
 import { DataQueryRequest, DataQueryResponse, DataSourceApi, DataSourceInstanceSettings } from '@grafana/ui';
 
-import { MyQuery, MyDataSourceOptions, defaultQuery } from './types';
-import { MutableDataFrame, FieldType} from '@grafana/data';
+import { defaultQuery, MyDataSourceOptions, MyQuery } from './types';
+import { FieldType, MISSING_VALUE, MutableDataFrame } from '@grafana/data';
 
 export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
   private settings: DataSourceInstanceSettings<MyDataSourceOptions>;
@@ -41,41 +41,85 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
   }
 
 
-  createLogTopicMappings(functions: any[]): Map<string, any[]> {
+  createLogTopicMappings(clientId: number, functions: any[]): Map<string, any[]> {
     let fmap: Map<string, any[]> = new Map();
     functions.map((fn) => {
-      if (fmap[fn.meta.topic_read] != null) {
-        fmap[fn.meta.topic_read].push(fn)
+      if(fn.meta.topic_read == null){
+        return;
+      }
+      let topicRead = String(clientId) + "/" + fn.meta.topic_read;
+      if (fmap[topicRead] != null) {
+        fmap[topicRead].push(fn)
       } else {
-        fmap[fn.meta.topic_read] = [fn];
+        fmap.set(topicRead, [fn]);
       }
     });
     return fmap;
   }
 
-  query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
+  fetchLog(installationId: number, from: number, to: number, topics?: string[]): any {
+    const url = this.settings.jsonData.url + "/api/v2beta/log/" + String(installationId);
+    let queryParams = {
+      "from": String(from),
+      "to": String(to),
+    };
+    if(topics) {
+      queryParams["topics"] = topics.join(",");
+    }
+    let queryString = "?" + Object.keys(queryParams).map(key => encodeURIComponent(key) + "=" + encodeURIComponent(queryParams[key])).join("&");
+    return fetch(url + queryString, {
+      headers: {
+        Authorization: 'Basic ' + btoa('grafana:' + this.settings.jsonData.apiKey),
+      },
+    }).then(result => result.json());
+  }
+
+  async query(options: DataQueryRequest<MyQuery>): Promise<DataQueryResponse> {
     const { range } = options;
     const from = range.from.valueOf();
     const to = range.to.valueOf();
 
-    // Return a constant for each query
-    const data = options.targets.map(target => {
+    let data = new Array<any>();
+    for(let target of options.targets) {
       const query = defaults(target, defaultQuery);
-      this.fetchFilteredFunctions(query.installationId, query.meta).then(data => {
-        let mappings = this.createLogTopicMappings(data);
-        console.log(mappings);
-
-      });
-
-      return new MutableDataFrame({
+      let filteredFunctions = await this.fetchFilteredFunctions(query.installationId, query.meta);
+      console.log(filteredFunctions);
+      let mappings = this.createLogTopicMappings(query.clientId, filteredFunctions);
+      let log = await this.fetchLog(query.installationId, from / 1000, to / 1000, Array.from(mappings.keys()));
+      const res = new MutableDataFrame({
         refId: query.refId,
-        fields: [
-          { name: 'Time', values: [from, to], type: FieldType.time },
-          { name: 'Value', values: [query.constant, query.constant], type: FieldType.number },
-        ],
+        fields: []
       });
-    });
+      res.addField({name: "Time", type:FieldType.time});
+      for(let fn of filteredFunctions) {
+        res.addField({
+          name: fn.meta.name,
+          type: FieldType.number,
+        });
+      }
 
+      console.log(mappings);
+
+      for(let entry of log.data) {
+        let row = new Array<any>();
+        row.push(entry.time * 1000);
+        let currentFn = mappings.get(entry.topic);
+        if (currentFn == null) {
+          continue;
+        }
+        console.log(currentFn);
+        for(let fn of filteredFunctions) {
+          if (fn.id == currentFn[0].id) {
+            row.push(entry.value);
+          } else {
+            row.push(MISSING_VALUE);
+          }
+        }
+        res.appendRow(row);
+      }
+      console.log(res);
+      data.push(res);
+    }
     return Promise.resolve({ data });
   }
 
